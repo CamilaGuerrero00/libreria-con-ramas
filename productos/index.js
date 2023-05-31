05b77b19e5ee34d3ab386ff02424692346b1d4b9
CamilaGuerrero00 <camilag.main@gmail.com>
2023-05-31 15:31:15 -0300

    funcionalidad principal

---
+++ productos/index.js
@@ -0,0 +1,17 @@
+//const{leerJSON,escribirJSON} =require('../data')
+//const Producto = require('./Producto')
+
+module.exports = {
+ // productos : leerJSON(),
+
+  //listar : require('./listar'),
+
+  //agregar : require('./agregar') ,
+  
+ // filtrar : require('./filtrar') ,
+
+ // editar : require('./ediar') ,
+
+ // buscar : require('./buscar')
+
+};
